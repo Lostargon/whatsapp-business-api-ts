ff635ae773292106319ef7698df08ccb1c98334b
lostargon <lostargon@gmail.com>
2023-08-01 12:50:58 +0200

    renamed wa to Server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * as Server from './api/WhatsAppApi';
+export {WhatsAppApi as Server} from './api/WhatsAppApi';
 export * as messageModels from './models/MessageModels';
 export * as webhookModels from './models/WebhookModels';
